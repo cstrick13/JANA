ad6b8f75435621998c475a33428d939c256c6521
Cory Strickland <corystrickland13@gmail.com>
2025-01-08 20:19:16 -0500

    Basic Wizard Layout Setup

--- Jana/src/app/app.component.ts
+++ Jana/src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { invoke } from "@tauri-apps/api/core";
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,RouterModule],
+  imports: [CommonModule, RouterOutlet,RouterModule,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(public router: Router) {}
+
   greetingMessage = "";
 
   greet(event: SubmitEvent, name: string): void {
