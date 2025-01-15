d760b7288aea0b6a91d64a7e576a26e41992e669
Cory Strickland <corystrickland13@gmail.com>
2025-01-14 19:53:12 -0500

    Made changes to splash page

--- Jana/src/app/wizard-2/wizard-2.component.ts
+++ Jana/src/app/wizard-2/wizard-2.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-wizard-2',
   standalone: true,
@@ -10,5 +11,41 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   styleUrl: './wizard-2.component.css'
 })
 export class Wizard2Component {
+  playAudio() {
+    try {
+      const audio = new Audio('/assets/voices/audio1.mp3'); // Ensure the path is correct
+      audio.play();
+      console.log('Audio file is playing.');
+    } catch (error) {
+      console.error('Failed to play audio file:', error);
+    }
+  }
+  playAudio2() {  
+    try {
+      const audio = new Audio('/assets/voices/audio2.mp3'); // Ensure the path is correct
+      audio.play();
+      console.log('Audio file is playing.');
+    } catch (error) {
+      console.error('Failed to play audio file:', error);
+    }
+  }
 
+  playAudio3() {  
+    try {
+      const audio = new Audio('/assets/voices/audio3.mp3'); // Ensure the path is correct
+      audio.play();
+      console.log('Audio file is playing.');
+    } catch (error) {
+      console.error('Failed to play audio file:', error);
+    }
+  }
+  playAudio4() {  
+    try {
+      const audio = new Audio('/assets/voices/audio4.mp3'); // Ensure the path is correct
+      audio.play();
+      console.log('Audio file is playing.');
+    } catch (error) {
+      console.error('Failed to play audio file:', error);
+    }
+  }
 }
