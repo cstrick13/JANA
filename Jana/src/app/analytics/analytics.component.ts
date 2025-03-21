a8384587eb555e914ef18bd29d8defc622bfe3de
D-Michiels <daltonmichiels@yahoo.com>
2025-03-21 15:47:56 -0500

    Added new widgets
    Some of the data is hard coded currently, just need to hook up the aruba switch to do more.

--- Jana/src/app/analytics/analytics.component.ts
+++ Jana/src/app/analytics/analytics.component.ts
@@ -1,19 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-analytics',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatProgressBarModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatIconModule
+  ],
   templateUrl: './analytics.component.html',
   styleUrls: ['./analytics.component.css']
 })
 export class AnalyticsComponent {
-  @Input() title!: string;
-  @Input() utilization!: number;
-  @Input() moduleName!: string;
-  @Input() icon!: string;
+  // CPU & Memory Data
+  cpuUtilization = 50;
+  memoryUtilization = 91;
+  moduleName = 'Mgmt Module 1/1';
+
+  // Firmware Data
+  currentVersion = "FL.10.12.1021";
+  primaryVersion = "FL.10.12.1021";
+  secondaryVersion = "FL.10.06.0120";
+
+  // Fans Data
+  totalFans = 3;
+  criticalFans = 0;
+  warningFans = 0;
+
+  // Logs Data
+  criticalLogs = 0;
+  warningLogs = 0;
 }
