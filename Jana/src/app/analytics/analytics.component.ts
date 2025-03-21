58a9dc6a28046aaa4cdd88099c24597232d7f3ea
D-Michiels <daltonmichiels@yahoo.com>
2025-03-20 23:22:43 -0500

    Got the first widget up
    Got the first widget up, need to figure out how to connect it to the API to grab the constant data from the switch.

--- Jana/src/app/analytics/analytics.component.ts
+++ Jana/src/app/analytics/analytics.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-analytics',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatProgressBarModule, MatIconModule],
   templateUrl: './analytics.component.html',
-  styleUrl: './analytics.component.css',
-  standalone: false
+  styleUrls: ['./analytics.component.css']
 })
 export class AnalyticsComponent {
-
+  @Input() title!: string;
+  @Input() utilization!: number;
+  @Input() moduleName!: string;
+  @Input() icon!: string;
 }
